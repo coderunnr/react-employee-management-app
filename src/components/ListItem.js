6460f00db33958d1dca0a59296b93ee9ccb20ce1
Kanishk Sharma <kanishksharma3012@gmail.com>
2018-04-12 13:02:59 +0530

    employee edit and text schedule to employee add

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,7 +6,7 @@ import { CardSection } from './common';
 class ListItem extends Component {
 
     onRowPressed() {
-        Actions.employeeCreate({ employee: this.props.employee });
+        Actions.employeeEdit({ employee: this.props.employee });
     }
 
     render() {
